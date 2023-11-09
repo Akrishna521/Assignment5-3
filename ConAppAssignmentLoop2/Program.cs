7327ace039c11d9b80a46c8b9c18561f7ffa967a
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-11-09 15:04:25 +0530

    Assignment5-2

---
+++ ConAppAssignmentLoop2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class FibonacciExample
+{
+    public static void Main(string[] args)
+    {
+        int n = 10;
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
